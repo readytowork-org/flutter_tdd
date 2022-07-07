2450983ac4f56f7114dd9c126dd2587cae382d7a
jaswinda <singhjassi2624@gmail.com>
2022-07-07 14:12:44 +0545

    feat: Unit testing Using provider

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tdd/screens/favorite_page.dart';
+import 'package:flutter_tdd/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'models/favorites.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return (TestingApp());
+  }
+}
+
+class TestingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          HomePage.routeName: (context) => HomePage(),
+          FavoritesPage.routeName: (context) => FavoritesPage(),
+        },
+        initialRoute: HomePage.routeName,
+      ),
+    );
+  }
+}
